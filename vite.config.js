c28f45be7206759c51e8f398cf7de50d5271ee34
voltexman <voltexman@gmail.com>
2023-09-30 00:39:51 +0300

    seo

--- vite.config.js
+++ vite.config.js
@@ -6,8 +6,6 @@ export default defineConfig({
     plugins: [
         laravel({
             input: [
-                'resources/css/app.css',
-                'resources/js/app.js',
                 'resources/css/pages/main.css',
                 'resources/js/pages/main.js',
                 'resources/css/pages/articles.css',
