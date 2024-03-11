03da34ea3a98839a68b8536dab3c833cf05f4939
voltexman <voltexman@gmail.com>
2024-03-11 21:19:29 +0200

    mobile menu styled

--- resources/views/components/input.blade.php
+++ resources/views/components/input.blade.php
@@ -3,7 +3,7 @@
 <div x-data="{ counter: false }" class="relative">
 
     @isset($icon)
-        @svg('lucide-' . $icon, 'absolute w-5 h-5 top-4 left-4 opacity-80')
+        @svg('lucide-' . $icon, 'absolute w-5 h-5 top-4 left-4 text-max-soft opacity-90')
     @endisset
 
     <input
--- resources/views/components/menu.blade.php
+++ resources/views/components/menu.blade.php
@@ -1,8 +1,9 @@
-<ul class="self-center mx-auto lg:flex">
+<ul class="self-center mx-auto lg:flex lg:gap-x-10">
     @foreach ($items as $item)
         @if ($item['url'] !== url()->current())
-            <li class="text-sm transition text-max-light hover:text-max-soft">
-                <a class="flex font-normal w-full items-center gap-x-2 py-2 px-2.5 rounded-lg" href="{{ $item['url'] }}">
+            <li class="mx-10 mb-3 text-sm transition lg:m-0 text-max-light hover:text-max-soft">
+                <a class="flex items-center w-full p-3 font-normal border rounded-lg ps-6 lg:p-0 lg:bg-transparent lg:border-0 gap-x-2 border-max-soft/20 bg-max-soft/10"
+                    href="{{ $item['url'] }}">
                     @svg('lucide-' . $item['icon'], 'inline-flex h-4 w-4')
                     {{ $item['label'] }}
                 </a>
