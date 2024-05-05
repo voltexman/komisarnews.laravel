5a16db4f025115dbf5152efe1b5f63dd64059d06
voltexman <voltexman@gmail.com>
2024-05-05 22:36:54 +0300

    fixed

--- resources/views/components/hair-sequence/desktop/item.blade.php
+++ resources/views/components/hair-sequence/desktop/item.blade.php
@@ -3,7 +3,11 @@
 <div class="flex flex-col items-center px-8 py-10 transition-shadow bg-white rounded-lg shadow-md hover:shadow-xl">
     <img data-src="{{ asset("images/icons/{$icon}.svg") }}" class="w-20 h-20 lazyload" alt="">
 
-    <h2 class="mt-8 mb-2 font-bold text-center uppercase">title</h2>
+    @isset($title)
+        <h2 class="mt-8 mb-2 font-bold text-center uppercase">{{ $title }}</h2>
+    @endisset
 
-    <div class="">description</div>
+    @isset($description)
+        <div class="">{{ $description }}</div>
+    @endisset
 </div>
