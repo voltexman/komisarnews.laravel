b7b549ce96d2c4909f3b0fa390eeed49ff648410
voltexman <voltexman@gmail.com>
2024-05-21 14:41:45 +0300

    fixed

--- app/Filament/Resources/PostResource.php
+++ app/Filament/Resources/PostResource.php
@@ -56,7 +56,7 @@ class PostResource extends Resource
                             SpatieMediaLibraryFileUpload::make('image')
                                 ->collection('posts')
                                 ->conversion('preview')
-                                ->disk('media')
+                                // ->disk('media')
                                 ->imageEditor()
                                 ->imageEditorMode(3)
                                 ->image()
