412f5e515398cb931f9d1b1237a9d91aab7e2026
voltexman <voltexman@gmail.com>
2024-08-14 14:17:35 +0300

    fixed

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -46,12 +46,12 @@
                 <p>Жителі багатьох міст можуть <x-badge>продати волосся</x-badge> особисто <x-badge>нам в руки!</x-badge>
                     Подаруєте собі настрій, не бійтеся мінятися і удосконалюватися, продаючи свої коси, ви робите благу
                     справу і заробляєте додаткові гроші на потрібні покупки.</p>
-                <img src="{{ asset('images/about.jpg') }}"
+                <img src="{{ Vite::asset('images/about.jpg') }}"
                     class="object-cover w-full mt-8 mb-8 shadow-lg rounded-3xl h-36 lg:animate-none animate-slide-top-to-bottom shadow-max-black/30 lg:mb-0 lg:h-72"
                     alt="">
             </div>
             <div class="col-span-3 leading-5">
-                <img src="{{ asset('images/about2.jpg') }}"
+                <img src="{{ Vite::asset('images/about-2.jpg') }}"
                     class="hidden object-cover w-full mb-8 shadow-lg rounded-3xl animate-slide-bottom-to-top h-72 lg:block shadow-max-black/30"
                     alt="">
                 <p>Звертайтеся за консультацією прямо зараз за нашими контактами. Ми завжди готові відповісти на будь-які
