f0c5321ca2da1d93b092f0ab691ec125076f1286
voltexman <voltexman@gmail.com>
2024-08-14 14:22:23 +0300

    fixed

--- resources/views/components/hair-sequence/desktop/item.blade.php
+++ resources/views/components/hair-sequence/desktop/item.blade.php
@@ -1,7 +1,7 @@
 @props(['image', 'title', 'description'])
 
 <div
-    class="relative flex flex-col items-center overflow-hidden transition-shadow duration-300 rounded-lg shadow-lg bg-max-soft/15 hover:shadow-xl group group-hover:shadow-max-soft/80">
+    class="relative flex flex-col items-center overflow-hidden transition-shadow duration-300 rounded-xl shadow-lg bg-max-soft/15 hover:shadow-xl group group-hover:shadow-max-soft/80">
     <img data-src="{{ Vite::asset('resources/images/' . $image) }}" class="object-cover size-full lazyload" alt="">
     <div
         class="absolute bottom-0 transition-[height] w-full h-16 p-5 duration-300 backdrop-blur-sm bg-max-soft/60 group-hover:h-40 group-hover:bg-max-dark/60 group-hover:backdrop-blur-md">
@@ -15,7 +15,7 @@
 
         <div
             class="leading-5 text-white transition duration-500 ease-out delay-300 translate-y-56 group-hover:translate-y-0">
-            {{ $description }}</div>
-
+            {{ $description }}
+        </div>
     </div>
 </div>
